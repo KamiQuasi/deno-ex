cbe7bae77a85431aacfb298e3726c88af75dc392
Luke Dary <lukedary@gmail.com>
2021-04-07 19:16:40 -0400

    First Code

---
+++ app.ts
@@ -0,0 +1,72 @@
+import { Application, Router, HttpError, Status, send } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+const router = new Router();
+const scriptCache = new Map<string,string>();
+
+// Error Handling
+app.use(async (context, next) => {
+  try {
+    await next();
+  } catch (e) {
+    if (e instanceof HttpError) {
+      context.response.status = e.status as any;
+      if (e.expose) {
+        context.response.body = `<!doctype html><html><body><h1>${e.status} - ${e.message}</h1></body></html>`;
+      } else {
+        context.response.body = `<!doctype html><html><body><h1>${e.status} - ${Status[e.status]}</h1></body></html>`;
+      }
+    } else if (e instanceof Error) {
+      context.response.status = 500;
+      context.response.body = `<!doctype html><html><body><h1>500 - Internal Server Error</h1></body></html>`;
+      console.log("Unhandled Error:", e.message);
+      console.log(e.stack);
+    }
+  }
+});
+
+// Request Routing
+router
+  .get("/", ctx=> {
+    ctx.response.type = 'text/html; charset=utf-8';
+    ctx.response.body = `<!doctype html><html><body>Deno Example</body></html>`;
+  })
+  .get("/assets/scripts/:path+", async ctx => {
+    const fileName = `./assets/scripts/${ctx.params['path']}`;
+    ctx.response.type = 'application/javascript';
+    if (scriptCache.has(fileName)) {
+      ctx.response.body = scriptCache.get(fileName);
+    } else if (await exists(fileName.replace('.js','.ts'))) {
+      const { files, diagnostics } = await Deno.emit(fileName.replace('.js','.ts'), {
+        check: false,
+        bundle: 'esm',
+        compilerOptions: {
+          lib: ['es6','dom'],
+          module: 'es6',
+          target: 'es6'
+        }
+      });
+      if (diagnostics.length) {
+        console.warn(Deno.formatDiagnostics(diagnostics));
+      }
+      scriptCache.set(fileName, files['deno:///bundle.js']);
+      ctx.response.body = scriptCache.get(fileName);
+    } else {
+      ctx.response.body = '';
+    }
+  })
+  .get("/assets/:path+", async ctx => {
+    await send(ctx, ctx.request.url.pathname, {
+      root: `${Deno.cwd()}`
+    });
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener('listen', ({hostname, port}) => {
+  console.log(`Serving ${Deno.cwd()}`);
+  console.log(`Start listening on ${hostname}:${port}`);
+});
+
+await app.listen({hostname: "0.0.0.0", port: 8080 });
